f60c8b74d8adf8d8c118fe33ccfc52448de102a1
Sergey Badin <sergeykfg1@me.com>
2026-02-09 11:33:08 +0100

    add gallery page

---
+++ pages/gallery.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+export default function GalleryPage() {
+  return (
+    <main style={{ background: "#000", minHeight: "100vh", color: "#fff" }}>
+      <h1>Gallery</h1>
+    </main>
+  );
+}
