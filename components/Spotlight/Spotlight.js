fa4853af8977b75d47b170e8f91175bce90590ce
Ramon <ramon2@isbarn.net>
2026-02-09 15:00:06 +0100

    implement suggested changes

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -2,31 +2,15 @@ import useSWR from "swr";
 import Image from "next/image";
 import styled from "styled-components";
 
-const URL = "https://example-apis.vercel.app/api/art";
-
-async function fetcher(url) {
-  const response = await fetch(url);
-  return await response.json();
-}
-
 export default function Spotlight() {
-  const { data, error, isLoading } = useSWR(URL, fetcher);
+  const URL = "https://example-apis.vercel.app/api/art";
+
+  const { data, error, isLoading } = useSWR(URL);
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
 
-  console.log(data);
-  const randomObject = data[Math.floor(Math.random() * 11)];
-
-  const StyledImage = styled(Image)`
-    object-fit: cover;
-  `;
-
-  const StyledContainer = styled.div`
-    width: 50vw;
-    height: 50vh;
-    position: relative;
-  `;
+  const randomObject = data[Math.floor(Math.random() * data.length)];
 
   return (
     <>
@@ -35,11 +19,18 @@ export default function Spotlight() {
           fill={true}
           src={randomObject.imageSource}
           alt={randomObject.name}
-          // width={randomObject.dimensions.width}
-          // height={randomObject.dimensions.height}
         />
       </StyledContainer>
       <p>{randomObject.artist}</p>
     </>
   );
 }
+const StyledImage = styled(Image)`
+  object-fit: cover;
+`;
+
+const StyledContainer = styled.div`
+  width: 50vw;
+  height: 50vh;
+  position: relative;
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
