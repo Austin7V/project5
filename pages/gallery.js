764032b878b22a3222cb9713da8aad107d1a3db9
Sergey Badin <sergeykfg1@me.com>
2026-02-09 11:57:10 +0100

    add fetch gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,8 +1,26 @@
 import { useEffect, useState } from "react";
 export default function GalleryPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const urlArt = "https://example-apis.vercel.app/api/art";
+
+  useEffect(() => {
+    async function fetchArt() {
+      console.log("fetch art!");
+
+      const gallery = await fetch(urlArt);
+      const galleryData = await gallery.json();
+
+      console.log(galleryData);
+
+      setIsLoading(false);
+    }
+    fetchArt();
+  });
+
   return (
     <main style={{ background: "#000", minHeight: "100vh", color: "#fff" }}>
       <h1>Gallery</h1>
+      {isLoading ? <p> Loading...</p> : <p> Loaded!</p>}
     </main>
   );
 }
