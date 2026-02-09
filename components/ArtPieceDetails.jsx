1fc26a4437102ebac9a0b6c16ab7148a84fc6278
Hannah Capell <hannah.capell@gmail.com>
2026-02-09 16:36:14 +0100

    ArtPieceDetailsPage function for dynamic pages

--- components/ArtPieceDetails.jsx
+++ components/ArtPieceDetails.jsx
@@ -5,11 +5,16 @@ export default function ArtPieceDetails({ piece }) {
   return (
     <div>
       <h1>{piece.name}</h1>
-      <Image src={piece.imageSource} alt={piece.name} width={400} height={400} />
+      <Image
+        src={piece.imageSource}
+        alt={piece.name}
+        width={400}
+        height={400}
+      />
       <p>Artist: {piece.artist}</p>
       <p>Year: {piece.year}</p>
       <p>Genre: {piece.genre}</p>
-      <button>Back</button>
+      <button onClick={() => Router.push("/gallery")}>Back</button>
     </div>
   );
 }
