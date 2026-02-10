e3f5c2073408eb2550789e039d0bd4d4426ef15f
Ramon <ramon2@isbarn.net>
2026-02-10 15:43:08 +0100

    fix spotlight bug

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -1,22 +1,27 @@
 import Image from "next/image";
 import styled from "styled-components";
 import FavouritesButton from "../FavouritesButton/FavouritesButton";
+import { useEffect, useState } from "react";
 
 export default function Spotlight({ pieces, isLiked, onToggle }) {
-  const randomObject = pieces[Math.floor(Math.random() * pieces.length)];
+  const [spotlightPiece, setSpotlight] = useState(pieces[0]);
+
+  useEffect(() => {
+    setSpotlight(pieces[Math.floor(Math.random() * pieces.length)]);
+  }, [pieces]);
 
   return (
     <>
       <StyledContainer>
         <StyledImage
           fill={true}
-          src={randomObject.imageSource}
-          alt={randomObject.name}
+          src={spotlightPiece.imageSource}
+          alt={spotlightPiece.name}
         />
       </StyledContainer>
-      <p>{randomObject.artist}</p>
+      <p>{spotlightPiece.artist}</p>
       <FavouritesButton
-        id={randomObject.slug}
+        id={spotlightPiece.slug}
         isLiked={isLiked}
         onToggle={onToggle}
       />
