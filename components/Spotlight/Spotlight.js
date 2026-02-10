f13994604fa71f2d9a2ec697bdd61a8ef128a7cc
Sergey Badin <sergeykfg1@me.com>
2026-02-10 10:57:17 +0100

    update fetch

--- components/Spotlight/Spotlight.js
+++ components/Spotlight/Spotlight.js
@@ -1,16 +1,8 @@
-import useSWR from "swr";
 import Image from "next/image";
 import styled from "styled-components";
 
-export default function Spotlight() {
-  const URL = "https://example-apis.vercel.app/api/art";
-
-  const { data, error, isLoading } = useSWR(URL);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
-
-  const randomObject = data[Math.floor(Math.random() * data.length)];
+export default function Spotlight({ pieces }) {
+  const randomObject = pieces[Math.floor(Math.random() * pieces.length)];
 
   return (
     <>
