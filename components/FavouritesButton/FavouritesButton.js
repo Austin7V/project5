d9a64976393f796dbb51d41a64a6388d183f120c
Ramon <ramon2@isbarn.net>
2026-02-11 16:18:02 +0100

    complete artpiecedata-state for liked

--- components/FavouritesButton/FavouritesButton.js
+++ components/FavouritesButton/FavouritesButton.js
@@ -1,9 +1,9 @@
 export default function FavouritesButton({ slug, artPieceData, onToggle }) {
   // console.log(artPieceData.filter((artPiece) => artPiece.slug === slug.slug));
 
-  const isLiked = artPieceData?.find(
+  const isLiked = artPieceData.find(
     (artPiece) => artPiece.slug === slug
-  ).isLiked;
+  )?.isLiked;
   console.log(isLiked);
   return (
     <button
